3d8213dc66de1f3cc69c561f049ae8a1138abe45
tamasposta <tamas.posta@outlook.com>
2023-11-15 11:29:53 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require ('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : '',
+      entries: 0,
+      database : 'smart-brain'
+    }
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send(database.users) })
+app.post('/signin',     signin.handleSignin(db, bcrypt))
+app.post('/register',   register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image',       image.handleImage(db))
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+});
